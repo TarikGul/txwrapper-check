ce82ee4db519a66464778512a8e72df6f50e073d
tarikgul <tarik@parity.io>
2022-05-03 22:33:00 -0400

    cleanup call

--- call.ts
+++ call.ts
@@ -1,34 +1,21 @@
+import '@polkadot/api-augment';
 import { ApiPromise, WsProvider } from '@polkadot/api';
-import { typesBundle } from '@acala-network/type-definitions';
 
-
+/**
+ * Simple polkadot-js template to test and reproduce things locally
+ */
 const main = async () => {
     const api = await ApiPromise.create({
-        provider: new WsProvider('wss://karura-rpc-1.aca-api.network'),
-        typesBundle
+        provider: new WsProvider('wss://rpc.polkadot.io'),
     });
 
-    // Block 438151
-    const result = await api.rpc.chain.getBlock('0xcb7f7866a14f88bb19fd2b2c04303c381bd569c32413b7eac719e5ef8a40ba9c')
+    const apiAt = await api.at('0x29e2e4f443a33bc347bfdcc045765c62f859ae9a008b846079713fc5e56f50b2')
 
-    console.log(result.toHuman())
+    // Tests against v29
+    console.log('Using api.at: ', apiAt.consts.system.blockWeights.toJSON());
+
+    // Tests against v9180
+    console.log('Using api: ', api.consts.system.blockWeights.toJSON())
 };
 
-main().catch(err => console.log(err))
-
-// import { ApiPromise, WsProvider } from '@polkadot/api';
-// import { typesBundle } from 'moonbeam-types-bundle'
-
-// const main = async () => {
-//     const api = await ApiPromise.create({
-//         provider: new WsProvider('wss://wss.moonriver.moonbeam.network'),
-//         typesBundle
-//     });
-
-//     // Block 427542
-//     const result = await api.rpc.chain.getBlock('0x9caa779d514cac6d60e51797f88f05a2d5cb839bb6695f039e0810cf21c7b046')
-
-//     console.log(result.toHuman())
-// };
-
-// main().catch(err => console.log(err))
+main().catch(err => console.log(err)).finally(() => process.exit());
